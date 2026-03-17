48c73cfe8e1f4ed0cc5d2e6f713bd40f346a43b8
srikanth240695 <77737462+srikanth240695@users.noreply.github.com>
2026-03-17 12:12:28 +0530

    Update default project ID in variable.tf

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -1,7 +1,7 @@
 variable "project" {
   description = "this is gcp project-id"
   type        = string
-  default     = "rock-loop-449918-b0"
+  default     = "project-d42dcdc9-67e0-4029-8dd"
 }
 
 variable "region" {
